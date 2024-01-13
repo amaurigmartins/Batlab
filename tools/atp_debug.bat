ef949a83ee3a4a3a0a2eef68ed87fb1ab9b73cc9
Amauri Martins <amaurigmartins@gmail.com>
2024-01-13 11:05:51 -0300

    Delete tools directory

--- tools/atp_debug.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "desbloq_name=%1_DESBLOQ.ATP"
-copy %1 %desbloq_name%
-del %1
-rem move /y %desbloq_name% %1
-echo "Este script não executara o solver ATP, podendo ocorrer mensagens de erro no ATPDraw. Ao inves, o arquivo ATP desbloqueado sera aberto para edicao."
-pause
-notepad %desbloq_name%
--- tools/runATP_modif.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "desbloq_name=%1_DESBLOQ.ATP"
-copy %1 %desbloq_name%
-rem pause
-rem Find_And_Replace.vbs %desbloq_name% "IF (t <= timestep) THEN" "IF ((RG_PC = 0.0) OR (t <= timestep)) THEN"
-Find_And_Replace.vbs %desbloq_name% "DELAY CELLS DFLT: 1000" "DELAY CELLS DFLT: 50000"
-MOVE /Y %desbloq_name% %1
-REM BatchSubstitute.bat "IF (t <= timestep) THEN" "IF ((RG_PC = 0.0) OR (t <= timestep)) THEN" %1
-C:\ATP\tools\runATP.exe %1
